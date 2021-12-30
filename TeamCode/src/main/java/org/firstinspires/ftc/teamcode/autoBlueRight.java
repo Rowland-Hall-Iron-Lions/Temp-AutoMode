98ce57ad49f91d2d4f99a469c07bcfec0335d0a2
DragonDev07 <teowelton@gmail.com>
2021-12-30 13:45:50 -0700

    declared all motors and setup basis for trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoBlueRight.java
@@ -1,2 +1,45 @@
-package org.firstinspires.ftc.teamcode;public class autoBlueRight {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class autoBlueRight extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        Servo clawServo = hardwareMap.get(Servo.class, "clawServo");
+        DcMotor duckWheel = hardwareMap.get(DcMotor.class, "duckWheel");
+        CRServo intakeL = hardwareMap.get(CRServo.class, "intakeL");
+        CRServo intakeR = hardwareMap.get(CRServo.class, "intakeR");
+        DcMotor extender = hardwareMap.get(DcMotor.class, "extender");
+        DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
+
+        // Run using encoders. This allows us to count how many rotations
+        // each motor does and use that data accordingly
+        extender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        duckWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Make it so that the motors brake when they have zero power
+        extender.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        duckWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Set Directions of Motors
+        duckWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Declare Trajectories
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+    }
 }
