2533e479430ec8ac707c838f4443e8b3c3b3a6fd
DragonDev07 <teowelton@gmail.com>
2022-01-02 16:19:28 -0700

    File swap. Red Right is now in the correct file, same thing for Blue side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoBlueRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoBlueRight.java
@@ -40,25 +40,26 @@ public class autoBlueRight extends LinearOpMode {
         // Set Directions of Motors
         duckWheel.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Declare Trajectories
+        //Declare Trajectories
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         // Trajectory to get to the duck spinner from our starting position
-        Trajectory toDuckSpinner = drive.trajectoryBuilder(new Pose2d(-35, 58, 0))
-                .lineToLinearHeading(new Pose2d(-58, -58, Math.toRadians(0)))
+        Trajectory toDuckSpinner =  drive.trajectoryBuilder(new Pose2d(-35, 58, 0))
+                .lineToLinearHeading(new Pose2d(-58, 58, Math.toRadians(0)))
                 .build();
-        // Trajectory to park  our robot fully within the team "box". This scores extra points
+        // Trajectory to park  our robot fully within the team "box".
+        // This scores extra points
         Trajectory park = drive.trajectoryBuilder(drive.getPoseEstimate())
-                .strafeLeft(23)
+                .strafeRight(23)
                 .build();
 
-        // Run while the Autonomous mode is active
+        // Run While the Autonomous Mode is Active
         waitForStart();
-        while (opModeIsActive()) {
-            // Update telemetry status to show that it is Running
+        while(opModeIsActive()){
+            // Update telemetry status to show that it is running
             telemetry.addData("Status", "Running");
             telemetry.update();
 
-            // Follow Trajectories. This is what the robot will actually do
+            // Follow trajectories. This is what the robot will actually do
             drive.followTrajectory(toDuckSpinner);
             duckWheel.setPower(25);
             sleep(5000);
@@ -66,7 +67,7 @@ public class autoBlueRight extends LinearOpMode {
             drive.followTrajectory(park);
             sleep(5000);
 
-            //Stop the Autonomous mode after we finish parking
+            // Stop the Autonomous Mode after we finish parking
             return;
         }
     }
